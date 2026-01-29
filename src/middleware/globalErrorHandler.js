5ea7423ac1c9d179c017119a7de581dd86dffbbe
ahhmedsamii77 <ahhmddsamii77@gmail.com>
2026-01-29 18:00:15 +0200

    drop me final update2

--- src/middleware/globalErrorHandler.js
+++ src/middleware/globalErrorHandler.js
@@ -1,7 +1,6 @@
 function globalErrorHandler(error, req, res, next) {
   return res.status(error.statusCode || 500).json({ 
     message: error.message, 
-    stack: process.env.NODE_ENV === "development" ? error.stack : null
   });
 }
 
