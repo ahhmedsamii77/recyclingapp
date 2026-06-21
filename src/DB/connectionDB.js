9a3ce0898a1f94e33feb18fe67a37b85d621b858
ahhmedsamii77 <ahhmddsamii77@gmail.com>
2026-06-21 09:32:55 +0300

    submit-1

--- src/DB/connectionDB.js
+++ src/DB/connectionDB.js
@@ -1,40 +1,36 @@
 import mongoose from "mongoose";
 
-async function connectionDB() {
-  mongoose.connect(process.env.DB_URL)
-    .then(() => console.log("DB connected..................."))
-    .catch((err) => console.log(err));
+const MONGODB_URI = process.env.DB_URL;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define DB_URL in environment variables");
 }
 
-export { connectionDB,
- };
+// Cache the connection across serverless invocations (required for Vercel)
+let cached = global.mongoose;
 
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
+async function connectionDB() {
+  // Return existing connection if available
+  if (cached.conn) {
+    console.log("DB reusing cached connection");
+    return cached.conn;
+  }
 
+  // Create new connection promise if not already in progress
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((m) => {
+      console.log("DB connected...................");
+      return m;
+    });
+  }
 
-// import mongoose from "mongoose";
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
-// const MONGODB_URI = process.env.DB_URL;
+export { connectionDB };
 
-// if (!MONGODB_URI) {
-//   throw new Error("Please define DB_URL in environment variables");
-// }
-
-// let cached = global.mongoose;
-
-// if (!cached) {
-//   cached = global.mongoose = { conn: null, promise: null };
-// }
-
-// async function connectionDB() {
-//   if (cached.conn) return cached.conn;
-
-//   if (!cached.promise) {
-//     cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
-//   }
-
-//   cached.conn = await cached.promise;
-//   return cached.conn;
-// }
-
-// export { connectionDB };
